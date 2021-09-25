0823b1ccc9333ed9bf3d085153559f5853148f90
yfdong <dyao1111@163.com>
2021-09-25 21:40:12 +0800

    Update Integrator.cpp

--- CG/Integrator.cpp
+++ CG/Integrator.cpp
@@ -1,1 +1,25 @@
 
+
+template <typename Float, typename Spectrum>
+class PathIntegrator: public MonteCarloIntegrator <Float, Spectrum> 
+{
+public:
+  MTS_IMPORT_BASE(MonteCarloIntegrator, m_max_depth, m_rr_depth) 
+  MTS_IMPORT_TYPES(Scene, Sampler, Medium, Emitter, EmitterPtr, BSDF, BSDFPtr) 
+  
+  PathIntegrator(const Properties &props):Base(props) { } 
+  
+  std::pair<Spectrum, Mask> sample( const Scene *scene, Sampler *sampler, const RayDifferential3f &ray_, const Medium*, Float*, Mask active) const override
+  {
+    MTS_MASKED_FUNCTION(ProfilerPhase::SamplingIntegratorSample, active); 
+    
+    RayDifferential3f ray=ray_; 
+    
+    Float eta(1.f); 
+    
+    Float emission_weight(1.f); 
+    Spectrum throughput(1.f), result(0.f); 
+    
+    // First Intersection 
+    
+    SurfaceInteraction3f si=
